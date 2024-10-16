693f61d6fae3a54f54b99f0ffb60376b453eac47
hishitashah <hishita.shah@gmail.com>
2024-10-16 04:00:48 -0500

    welcome page frontend

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
 
   const renderArchedText = () => {
     const welcomeText = "Welcome to";
-    const arcRadius = 40; // Adjust the radius for the curvature
+    const arcRadius = 45; // Adjust the radius for the curvature
     const totalAngle = Math.PI; // 180 degrees for a half-circle
     const charAngleStep = totalAngle / (welcomeText.length - 1);
 
@@ -23,7 +23,7 @@ export default function TabLayout() {
       <View style={styles.archContainer}>
         {welcomeText.split('').map((char, index) => {
           const angle = (index - (welcomeText.length - 1) / 2) * charAngleStep; // Calculate the angle for each character
-          const rotateAngle = `${angle * (180 / Math.PI)}deg`; // Convert radians to degrees
+          const rotateAngle = `${angle * (145 / Math.PI)}deg`; // Convert radians to degrees
 
           return (
             <Text
@@ -100,12 +100,19 @@ export default function TabLayout() {
     <View style={styles.container}>
       {/* Top Section with Header */}
       <View style={styles.topSection}>
-
+        <Image
+          source={require('../../assets/images/greenTopLeft.png')}
+          style={styles.topLeftImg}
+        />
+        <Image
+          source={require('../../assets/images/greenTopRight.png')}
+          style={styles.topRightImg}
+        />
         {/* Arched "Welcome to" Text */}
         {renderArchedText()}
 
         {/* RecycleRoute Text */}
-        <Text style={tw`text-5xl font-semibold text-[#6B8068] mt-60`}>RecycleRoute!</Text>
+        <Text style={tw`text-5xl font-bold uppercase text-[#6B8068] tracking-wide mt-60`}>RECYCLE ROUTE!</Text>
 
         {/* Reduce, Reuse, Recycle Text */}
         <Text style={tw`text-2xl font-bold text-black mb-1 mt-4`}>Reduce, Reuse, Recycle.</Text>
@@ -206,6 +213,22 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     width: '30%', // Adjust width to fit three buttons
+  },
+    topLeftImg: {
+    position: 'absolute',
+    top: -40,
+    left: -25,
+    width: 220,
+    height: 220,
+    resizeMode: 'contain',
+  },
+  topRightImg: {
+    position: 'absolute',
+    top: -22,
+    right: -50,
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
   },
   bottomLeftImg: {
     position: 'absolute',
