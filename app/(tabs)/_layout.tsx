3b31eea7f29073cbadee368cd1c14317ab343e00
hishitashah <hishita.shah@gmail.com>
2024-11-08 22:58:10 -0600

    changed welcome to text

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,8 +36,8 @@ export default function TabLayout() {
 
   const renderArchedText = () => {
     const welcomeText = "Welcome to";
-    const arcRadius = 45;
-    const totalAngle = Math.PI;
+    const arcRadius = 60;
+    const totalAngle = Math.PI*.65;
     const charAngleStep = totalAngle / (welcomeText.length - 1);
 
     return (
@@ -122,8 +122,8 @@ export default function TabLayout() {
         
         {renderArchedText()}
         <Text style={[tw`text-8xl font-bold text-[#6B8068] tracking-wide mb--20 mt-73`, { fontFamily: 'Nerko-One'}]}>ReQuest!</Text>
-        <Text style={[tw`text-4xl font-bold text-white mt-25`, {fontFamily: 'Gilroy'}]}>Reduce, Reuse, Recycle.</Text>
-        <Text style={[tw`text-4xl font-bold text-white mb--20`, {fontFamily: 'Gilroy'}]}>The Power is Yours!</Text>
+        <Text style={[tw`text-3xl font-bold text-white mt-25`, {fontFamily: 'Gilroy'}]}>Reduce, Reuse, Recycle.</Text>
+        <Text style={[tw`text-3xl font-bold text-white mb--20`, {fontFamily: 'Gilroy'}]}>The Power is Yours!</Text>
       </SafeAreaView>
       <View>
 <Svg
